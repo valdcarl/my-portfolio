f47c3087e317e823a51bf81b1cda58a6cdce10f0
Carlos V <77943334+valdcarl@users.noreply.github.com>
2025-02-28 20:17:50 -0700

    -Installation of Tailwind CSS -Configured to Vite plugin -Import tailwind css to .css file -Added h2 with a className of 'text-emerald-700' to test proper installation/configure

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2 className='text-emerald-700'>By: Carlos A. Valdez</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
