e6890ce7965e98d9f0ff9cfe6f41bd5ecdc3d3cc
Carlos V <77943334+valdcarl@users.noreply.github.com>
2025-03-09 12:39:35 -0600

    -Removed the boilerplate count onClick method when booting application

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,7 +9,6 @@ import Timeline from './components/Timeline'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
