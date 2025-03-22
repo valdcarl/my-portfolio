d96299246ae0868e51b2706a9fdd77ad7fe2d6c2
Carlos V <77943334+valdcarl@users.noreply.github.com>
2025-03-22 15:24:13 -0600

    Created components folder within' src & started Hero.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Hero from './components/Hero'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,21 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p>
-          Testing Carlos A. Valdez development branch
-        </p>
+
+      <div>
+        <Hero></Hero>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hero() {
+    return (
+        <div>
+            Test Test Hero Section
+        </div>
+
+    )
+}
+
+export default Hero;
