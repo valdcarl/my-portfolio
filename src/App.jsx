8c24d009c84f4cd89654c5b1c49b62d913b671da
Carlos V <77943334+valdcarl@users.noreply.github.com>
2025-03-22 21:28:43 -0600

    v1 completion of Hero.jsx and Hero.css in components section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Hero from './components/Hero'
 
@@ -8,18 +8,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    {/* Setting up routes */}
+      <Router>
+        <Routes>
+            <Route path="/" element={<Hero />} />
+        </Routes>
+      </Router>
 
-      <div>
-        <Hero></Hero>
-      </div>
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import "./Hero.css"
 
 function Hero() {
     return (
-        <div>
-            Test Test Hero Section
-        </div>
+        <section id="hero-section" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section--content">
+                    <p className="hero--section--title">Hello!, I'm Carlos!</p>
+                    <h1 className="hero--section--title--header">
+                        <span className="hero--section--title--colored">
+                            Let's Build
+                        </span>{" "}
+                        <br/>
+                        Together!
+                    </h1>
+                    <p className="hero--section--description">
+                        Passionate about technology from various disciplines under the Computer Science umbrella!  
+                        Including Software Engineering, Data Science, Data Analytics, AI, ML, Devops, Mobile 
+                        and so much more, our skills are meant to be expansive into multiple domains!
+                    </p>
+                </div>
+                <button className="hero--section--button button--primary">
+                    <a className="hero--section--button-atag"href="https://www.linkedin.com/in/carlos-valdez-cv/">
+                    Let's Talk!
+                    </a>
+                </button>
+            </div>
+            <div className="hero--section--img">
+                <img src={"./src/assets/profile-image-no-bg.png"} alt="My Profile Image"/>
+            </div>
+        </section>
 
     )
 }
