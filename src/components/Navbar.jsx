c0c050a924f9b40fe2a9d75c328c0304f191f37b
Carlos V <77943334+valdcarl@users.noreply.github.com>
2025-04-12 20:44:12 -0600

    - Navbar changes, more appealing, could use some work down the line, but good v1 to move onto other important components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,11 @@ import "./Navbar.css"
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
+        <nav className="navbar--main">
+            <div className="navbar--logo">
+                CARLOS VALDEZ
+            </div>
+            <ul className="navbar--itemlinks">
                 <li><a href="#hero">Home</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
